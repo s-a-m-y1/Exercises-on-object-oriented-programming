f2d936c89798defe7d02fa20c708c9cb6d34a22a
s-a-m-y1 <sam858y@gmail.com>
2026-02-13 11:45:30 +0200

    Edit RegsterLogin

--- clsLoginiformation.h
+++ clsLoginiformation.h
@@ -1,41 +1,41 @@
-#include<iostream>
-#include<fstream>
-#include"clsString.h"
-#include"clsDate.h"
-#include"clsTime.h"
-#include"Global.h"
-using namespace std;
-class  clsLoginiformation
-{
-    private:
+// #include<iostream>
+// #include<fstream>
+// #include"clsString.h"
+// #include"clsDate.h"
+// #include"clsTime.h"
+// #include"Global.h"
+// using namespace std;
+// class  clsLoginiformation
+// {
+//     private:
 
 
-    static string _convetLine()
-    {
-        string Line ="";
-        Line+=to_string( Date.GetDay())+"/"+ to_string( Date.GetMonth())+"/"+ to_string( Date.GetYear())+"/"+clsTime::GetCurrentTime()+"  "+Current.GetUserName()+"  "+Current.GetPassword()+"  "+ to_string( Current.GetPermisson() );        
-        return Line;
-    }
+//     static string _convetLine()
+//     {
+//         string Line ="";
+//         Line+=to_string( Date.GetDay())+"/"+ to_string( Date.GetMonth())+"/"+ to_string( Date.GetYear())+"/"+clsTime::GetCurrentTime()+"  "+Current.GetUserName()+"  "+Current.GetPassword()+"  "+ to_string( Current.GetPermisson() );        
+//         return Line;
+//     }
 
-    static void Addsessions()
-    {
-     fstream load;
-     load.open("sessions.txt",ios::app|ios::out);// Add sessions
-     if (load.is_open())
-     {
-        load<<_convetLine()<<endl;
+//     static void Addsessions()
+//     {
+//      fstream load;
+//      load.open("sessions.txt",ios::app|ios::out);// Add sessions
+//      if (load.is_open())
+//      {
+//         load<<_convetLine()<<endl;
         
-     }
-     load.close();
-    }
+//      }
+//      load.close();
+//     }
 
- public:
-static void Recordingsessions()
-{
-Addsessions();
+//  public:
+// static void Recordingsessions()
+// {
+// Addsessions();
 
-}
+// }
 
 
 
-};
+// };
